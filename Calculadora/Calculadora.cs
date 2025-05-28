b4f25472a698adbf3571e1f1e99a0043c2ccf26e
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-05-28 13:10:18 -0300

    Calculadora terminada

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -3,10 +3,9 @@ namespace EspacioCalculadora
     public class Calculadora
     {
         private double dato;
-        private double resultado;
 
         // Objeto Constructor, cuando creo la clase en new, el valor que mando se manda aca, sino le mando un parametro lo igualo a 0.
-        public Calculadora(double dato = 0) {
+        public Calculadora(double dato) {
             this.dato = dato;
         }
 
@@ -15,12 +14,27 @@ namespace EspacioCalculadora
             dato += termino;
         }
 
-        public void Dividir(double termino) {
-            if(termino != 0) dato = dato / termino;
+        public void Restar(double termino)
+        {
+            dato -= termino;
+        }
+
+        public void Multiplicar(double termino)
+        {
+            dato *= termino;
+        }
+        public void Dividir(double termino)
+        {
+            if (termino != 0) dato = dato / termino;
             else Console.WriteLine("No se puede dividir en 0");
         }
 
-        public double GetResultado() {
+        public void Limpiar()
+        {
+            dato = 0;
+        }
+        public double GetResultado()
+        {
             return dato;
         }
     }
